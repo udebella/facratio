b3f207146826a0e8235a70c65d4b7a237c83edb8
Ubu <ubald2b@gmail.com>
2017-12-23 16:27:15 +0100

    Temporarily comment that need to be refactored

--- src/model/base/base.spec.ts
+++ src/model/base/base.spec.ts
@@ -1,54 +1,54 @@
-import {FactoryType} from '../factoryType/factory-type';
-import {Item} from '../item/item';
-import {ItemQuantity} from '../itemQuantity/item-quantity';
-import {Recipe} from '../recipe/recipe';
-import {TimeFrame, TimeSpan} from '../timespan/timespan';
-
-// TODO Work in progress : need to refactor
-describe.skip('Base', () => {
-
-	describe('Method: findRecipes', () => {
-		const iron = new Item('Iron');
-		const oneIron =  new ItemQuantity(iron, 1);
-		const copper = new Item('Copper');
-		const twoCopper = new ItemQuantity(copper, 2);
-		const copperWire = new Item('Copper Wire');
-		const threeCopperWire = new ItemQuantity(copperWire, 3);
-		const greenCircuits = new Item('Green circuit');
-		const oneGreenCircuit = new ItemQuantity(greenCircuits, 1);
-		const halfASecond = new TimeSpan(0.5, TimeFrame.SECONDS);
-		let factoryType: FactoryType;
-		let copperWireRecipe: Recipe;
-		let greenCircuitRecipe: Recipe;
-
-		beforeEach(() => {
-			copperWireRecipe = new Recipe([oneIron], [twoCopper], halfASecond);
-			greenCircuitRecipe = new Recipe(
-				[threeCopperWire, oneIron],
-				[oneGreenCircuit], halfASecond);
-			factoryType = new FactoryType([copperWireRecipe, greenCircuitRecipe], 0.5);
-		});
-
-		// it('should list all recipes needed to convert input to output', () => {
-		// 	const greenCircuitsFlow = new ItemFlow(greenCircuits, 200);
-		// 	const base = new Base([iron, copper], [greenCircuitsFlow], [factoryType]);
-		// 	base.factories.forEach((factory) => {
-		// 		console.error(`Factory [${factory.computeNumberOfFactories()}:${factory.getFactoryEfficiency()}] :
-		// ${JSON.stringify(factory.getRealInput())} => ${JSON.stringify(factory.getRealOutput())}`);
-		// 	});
-		// 	expect(true).to.equal(true);
-		// });
-
-		// it('should list only needed recipes to convert input to output', () => {
-		// 	const copperWireFlow = new ItemFlow(copperWire, 1);
-		// 	const base = new Base([iron, copper], [copperWireFlow], [factoryType]);
-		// 	expect(base.findRecipes([copperWireFlow])).to.deep.equal([copperWireRecipe]);
-		// });
-	});
-
-	// describe('Method: combineRecipes', () => {
-	// 	it('should combine recipes', () => {
-    //
-	// 	});
-	// })
-});
+// import {FactoryType} from '../factoryType/factory-type';
+// import {Item} from '../item/item';
+// import {ItemQuantity} from '../itemQuantity/item-quantity';
+// import {Recipe} from '../recipe/recipe';
+// import {TimeFrame, TimeSpan} from '../timespan/timespan';
+//
+// // TODO Work in progress : need to refactor
+// describe.skip('Base', () => {
+//
+// 	describe('Method: findRecipes', () => {
+// 		const iron = new Item('Iron');
+// 		const oneIron =  new ItemQuantity(iron, 1);
+// 		const copper = new Item('Copper');
+// 		const twoCopper = new ItemQuantity(copper, 2);
+// 		const copperWire = new Item('Copper Wire');
+// 		const threeCopperWire = new ItemQuantity(copperWire, 3);
+// 		const greenCircuits = new Item('Green circuit');
+// 		const oneGreenCircuit = new ItemQuantity(greenCircuits, 1);
+// 		const halfASecond = new TimeSpan(0.5, TimeFrame.SECONDS);
+// 		let factoryType: FactoryType;
+// 		let copperWireRecipe: Recipe;
+// 		let greenCircuitRecipe: Recipe;
+//
+// 		beforeEach(() => {
+// 			copperWireRecipe = new Recipe([oneIron], [twoCopper], halfASecond);
+// 			greenCircuitRecipe = new Recipe(
+// 				[threeCopperWire, oneIron],
+// 				[oneGreenCircuit], halfASecond);
+// 			factoryType = new FactoryType([copperWireRecipe, greenCircuitRecipe], 0.5);
+// 		});
+//
+// 		// it('should list all recipes needed to convert input to output', () => {
+// 		// 	const greenCircuitsFlow = new ItemFlow(greenCircuits, 200);
+// 		// 	const base = new Base([iron, copper], [greenCircuitsFlow], [factoryType]);
+// 		// 	base.factories.forEach((factory) => {
+// 		// 		console.error(`Factory [${factory.computeNumberOfFactories()}:${factory.getFactoryEfficiency()}] :
+// 		// ${JSON.stringify(factory.getRealInput())} => ${JSON.stringify(factory.getRealOutput())}`);
+// 		// 	});
+// 		// 	expect(true).to.equal(true);
+// 		// });
+//
+// 		// it('should list only needed recipes to convert input to output', () => {
+// 		// 	const copperWireFlow = new ItemFlow(copperWire, 1);
+// 		// 	const base = new Base([iron, copper], [copperWireFlow], [factoryType]);
+// 		// 	expect(base.findRecipes([copperWireFlow])).to.deep.equal([copperWireRecipe]);
+// 		// });
+// 	});
+//
+// 	// describe('Method: combineRecipes', () => {
+// 	// 	it('should combine recipes', () => {
+//     //
+// 	// 	});
+// 	// })
+// });
--- src/model/base/base.ts
+++ src/model/base/base.ts
@@ -1,70 +1,70 @@
-import {Factory} from '../factory/factory';
-import {FactoryType} from '../factoryType/factory-type';
-import {Item} from '../item/item';
-import {ItemFlow} from '../itemFlow/item-flow';
-
-export class Base {
-	private input: Item[];
-	private output: ItemFlow[];
-	private factories: Factory[];
-	private authorizedTypes: FactoryType[];
-
-	constructor(input: Item[], output: ItemFlow[], authorizedTypes: FactoryType[]) {
-		this.input = input;
-		this.output = output;
-		this.authorizedTypes = authorizedTypes;
-		this.factories = [];
-		this.setUp(output);
-	}
-
-	private setUp(outputFlow: ItemFlow[]): void {
-		const itemsNeeded = outputFlow
-			.filter((itemFlow) => this.input.indexOf(itemFlow.getItem()) === -1);
-
-		if (itemsNeeded.length === 0) {
-			return;
-		}
-
-		const newOutput = itemsNeeded.reduce((previous, current) => {
-			const factoryType = this.authorizedTypes.find((type) => type.canProduce(current.getItem()));
-			const newFactory = new Factory(factoryType, [current]);
-			this.factories.push(newFactory);
-			return [
-				...previous,
-				...newFactory.getRealInput()
-			];
-		}, []);
-		return this.setUp(newOutput);
-	}
-
-	// public findRecipes(outputFlow: ItemFlow[], recipes: Recipe[] = []): Recipe[] {
-	// 	const itemsNeeded = outputFlow
-	// 		.filter((itemFlow) => this.input.indexOf(itemFlow.getItem()) === -1);
-	//
-	// 	if (itemsNeeded.length !== 0) {
-	// 		const newRecipes = this.authorizedTypes.reduce((previous: Recipe[], current: FactoryType) => {
-	// 			return [
-	// 				...previous,
-	// 				...this.findRecipesForFactoryType(current, itemsNeeded)
-	// 			];
-	// 		}, []);
-	//
-	// 		const newItemNeeded = newRecipes.reduce((previous, current) => {
-	// 			return [
-	// 				...previous,
-	// 				...current.getInput()
-	// 			];
-	// 		}, []);
-	// 		return this.findRecipes(newItemNeeded, [...recipes, ...newRecipes]);
-	// 	}
-	// 	return recipes;
-	// }
-	//
-	// private findRecipesForFactoryType(factoryType: FactoryType, outputFlow: ItemFlow[]): Recipe[] {
-	// 	return factoryType.getRecipes().filter((recipe) => {
-	// 		return recipe.getOutput()
-	// 			.map((output) => output.getItem())
-	// 			.find((item) => outputFlow.map((output) => output.getItem()).indexOf(item) !== -1);
-	// 	});
-	// }
-}
+// import {Factory} from '../factory/factory';
+// import {FactoryType} from '../factoryType/factory-type';
+// import {Item} from '../item/item';
+// import {ItemFlow} from '../itemFlow/item-flow';
+//
+// export class Base {
+// 	private input: Item[];
+// 	private output: ItemFlow[];
+// 	private factories: Factory[];
+// 	private authorizedTypes: FactoryType[];
+//
+// 	constructor(input: Item[], output: ItemFlow[], authorizedTypes: FactoryType[]) {
+// 		this.input = input;
+// 		this.output = output;
+// 		this.authorizedTypes = authorizedTypes;
+// 		this.factories = [];
+// 		this.setUp(output);
+// 	}
+//
+// 	private setUp(outputFlow: ItemFlow[]): void {
+// 		const itemsNeeded = outputFlow
+// 			.filter((itemFlow) => this.input.indexOf(itemFlow.getItem()) === -1);
+//
+// 		if (itemsNeeded.length === 0) {
+// 			return;
+// 		}
+//
+// 		const newOutput = itemsNeeded.reduce((previous, current) => {
+// 			const factoryType = this.authorizedTypes.find((type) => type.canProduce(current.getItem()));
+// 			const newFactory = new Factory(factoryType, [current]);
+// 			this.factories.push(newFactory);
+// 			return [
+// 				...previous,
+// 				...newFactory.getRealInput()
+// 			];
+// 		}, []);
+// 		return this.setUp(newOutput);
+// 	}
+//
+// 	// public findRecipes(outputFlow: ItemFlow[], recipes: Recipe[] = []): Recipe[] {
+// 	// 	const itemsNeeded = outputFlow
+// 	// 		.filter((itemFlow) => this.input.indexOf(itemFlow.getItem()) === -1);
+// 	//
+// 	// 	if (itemsNeeded.length !== 0) {
+// 	// 		const newRecipes = this.authorizedTypes.reduce((previous: Recipe[], current: FactoryType) => {
+// 	// 			return [
+// 	// 				...previous,
+// 	// 				...this.findRecipesForFactoryType(current, itemsNeeded)
+// 	// 			];
+// 	// 		}, []);
+// 	//
+// 	// 		const newItemNeeded = newRecipes.reduce((previous, current) => {
+// 	// 			return [
+// 	// 				...previous,
+// 	// 				...current.getInput()
+// 	// 			];
+// 	// 		}, []);
+// 	// 		return this.findRecipes(newItemNeeded, [...recipes, ...newRecipes]);
+// 	// 	}
+// 	// 	return recipes;
+// 	// }
+// 	//
+// 	// private findRecipesForFactoryType(factoryType: FactoryType, outputFlow: ItemFlow[]): Recipe[] {
+// 	// 	return factoryType.getRecipes().filter((recipe) => {
+// 	// 		return recipe.getOutput()
+// 	// 			.map((output) => output.getItem())
+// 	// 			.find((item) => outputFlow.map((output) => output.getItem()).indexOf(item) !== -1);
+// 	// 	});
+// 	// }
+// }
